413963405b11d18f5b7177470e93f0b37b639ee3
Irisboyev-I <irisboyev.ii@gmail.com>
2025-07-30 13:45:26 +0500

    Fix: Hero Section path to Articles

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -13,10 +13,10 @@ export default function Hero() {
                 {t('subtitle')}
             </p>
             <div className="flex justify-center gap-4">
-                <a href="/blog" className="text-[10px] md:text-xl bg-white text-blue-600 font-semibold py-2 px-6 rounded shadow hover:opacity-[.7] transition-[.66s]" >
+                <a href="/articles" className="text-[10px] md:text-xl bg-white text-blue-600 font-semibold py-2 px-6 rounded shadow hover:opacity-[.7] transition-[.66s]" >
                     {t('readBlog')}
                 </a>
-                <a href="/resources" className="md:text-xl text-[10px] bg-transparent border py-2 px-6 border-white font-semibold rounded hover:bg-white transition-[.66s] hover:text-blue-600" >
+                <a href="/articles" className="md:text-xl text-[10px] bg-transparent border py-2 px-6 border-white font-semibold rounded hover:bg-white transition-[.66s] hover:text-blue-600" >
                     {t('allResources')}
                 </a>
             </div>
